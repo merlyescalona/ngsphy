e3999d8a0a21375ec905d571193512f61f58cf0d
Merly Escalona <escalona10@gmail.com>
2017-08-15 20:56:41 +0200

    adding distributable version and scripts for building and installation testing.

--- scripts/dist-build-install.sh
+++ scripts/dist-build-install.sh
@@ -1,7 +1,11 @@
-#/bin/bash
+#!/bin/bash
 version="1.0.0"
 cd $HOME/git/ngsphy
 sudo rm -rf dist ngsphy.egg-info
 sudo rm -rf "/usr/local/lib/python2.7/dist-packages/ngsphy-${version}-py2.7.egg/ngsphy"
 sudo rm /usr/local/bin/ngsphy
 python setup.py sdist
+cd $HOME/git/ngsphy/dist
+tar -xzvf "ngsphy-${version}.tar.gz"
+cd "ngsphy-${version}"
+sudo python setup.py install
