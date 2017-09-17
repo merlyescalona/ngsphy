6cc649f1acc9131af6a3f11b5d1ff1e95998b1dd
Merly Escalona <escalona10@gmail.com>
2017-09-17 09:34:05 +0200

    adding full test case scenario for travis ci build

---
+++ scripts/travis.install.third.party.sw.ngsphy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Installing art"
+wget https://www.niehs.nih.gov/research/resources/assets/docs/artbinmountrainier20160605linux64tgz.tgz
+tar -zxvf  artbinmountrainier20160605linux64tgz.tgz
+export PATH=$PATH:$PWD/art_bin_MountRainier/
+git clone https://github.com/merlyescalona/indelible-ngsphy
+cd indelible-ngsphy && make
+export PATH=$PATH:$PWD/indelible-ngsphy/bin
