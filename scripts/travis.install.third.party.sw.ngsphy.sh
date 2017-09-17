3257dedd144505848a81eb42071aff4a3876e3c9
Merly Escalona <escalona10@gmail.com>
2017-09-17 09:49:07 +0200

    update pre-installation dependencies. missing popd in travis.yml

--- scripts/travis.install.third.party.sw.ngsphy.sh
+++ scripts/travis.install.third.party.sw.ngsphy.sh
@@ -4,5 +4,5 @@ wget https://www.niehs.nih.gov/research/resources/assets/docs/artbinmountrainier
 tar -zxvf  artbinmountrainier20160605linux64tgz.tgz
 export PATH=$PATH:$PWD/art_bin_MountRainier/
 git clone https://github.com/merlyescalona/indelible-ngsphy
-cd indelible-ngsphy && make
+cd indelible-ngsphy && make && popd
 export PATH=$PATH:$PWD/indelible-ngsphy/bin
